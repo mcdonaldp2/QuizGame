d48d8c572d5ddc9cb6f12187d8fa158df13d87d6
mcdonaldp2 <mcdonaldp2@vcu.edu>
2016-04-20 15:49:25 -0400

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Paul McDonald  on 4/20/16.
+//  Copyright © 2016 Paul McDonald . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
