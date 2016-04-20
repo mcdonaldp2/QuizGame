af0174066e9ddfaceaeb65050f8ea898ab95e747
William Crump <Ricky@Williams-Mac-mini.local>
2016-04-20 18:55:36 -0400

    UI stuff

---
+++ QuizGame/QuizViewController.swift
@@ -0,0 +1,42 @@
+//
+//  QuizViewController.swift
+//  QuizGame
+//
+//  Created by William Crump on 4/20/16.
+//  Copyright © 2016 Paul McDonald . All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var dButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- QuizGame/ViewController.swift
+++ QuizGame/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var singlePlayerButton: UIButton!
+    @IBOutlet weak var multiplayerButton: UIButton!
+    @IBOutlet weak var connectBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
